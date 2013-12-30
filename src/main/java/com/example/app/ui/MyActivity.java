458f611e6eedbff1ad05b20cd86e3d12b3a2d3d3
Turlough Cowman <turloughcowman@gmail.com>
2013-12-30 23:16:47 +0000

    SampleIntentService uses injected ITestable for log message

--- src/main/java/com/example/app/ui/MyActivity.java
+++ src/main/java/com/example/app/ui/MyActivity.java
@@ -1,7 +1,6 @@
 package com.example.app.ui;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
